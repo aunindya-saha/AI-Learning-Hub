c8ee87219cfcd90a5b1ef5194d630bf508728674
Aunindya Prosad Saha <anindosaha16@gmail.com>
2025-09-01 02:38:54 +0600

    Fix Vercel analytics: use stable version and add Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LearningHub from "./LearningHub.jsx";
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LearningHub />
+    <SpeedInsights />
     <Analytics />
   </React.StrictMode>
 );
