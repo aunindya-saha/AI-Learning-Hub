eaf3f069f9f09864fb363d58be85dc4e98348cf3
Aunindya Prosad Saha <anindosaha16@gmail.com>
2025-09-01 02:27:22 +0600

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LearningHub from "./LearningHub.jsx";
+import { Analytics } from "@vercel/analytics/next"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LearningHub />
+    <Analytics />
   </React.StrictMode>
 );
